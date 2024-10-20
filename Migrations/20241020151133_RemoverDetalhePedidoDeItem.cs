3b663896f5a0fdba903c8e7dda2a10886e4984d3
Vinícius <vorbitter@proton.me>
2024-10-20 16:59:08 -0300

    ADD:  Migrations/20241020151133_RemoverDetalhePedidoDeItem.cs and Migrations/20241020151133_RemoverDetalhePedidoDeItem.Designer.cs

---
+++ Migrations/20241020151133_RemoverDetalhePedidoDeItem.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CardapioOnline.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoverDetalhePedidoDeItem : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Nome",
+                table: "Categorias",
+                type: "TEXT",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "INTEGER");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Nome",
+                table: "Categorias",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "TEXT");
+        }
+    }
+}
