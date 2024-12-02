0938423c01ae6541bc5d9ec699b7ca640076e0f6
vinicius <vorbitter@proton.me>
2024-12-01 21:42:49 -0300

    Adicionados schemas

---
+++ CardapioOnline/Schemas/AdicionarDetalhePedidoSchema.cs
@@ -0,0 +1,8 @@
+namespace CardapioOnline.Schemas;
+
+public record AdicionarDetalhePedidoSchema
+{
+    public int Quantidade { get; set; }
+    public long PedidoId { get; set; }
+    public long ItemId { get; set; }
+}
