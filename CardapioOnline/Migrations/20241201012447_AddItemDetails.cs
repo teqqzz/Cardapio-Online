c8972d1e1776d2fa1e6fc609265c9c09ad0f6dcb
Nicollas Oliveira <nicollaskvasnei@gmail.com>
2024-12-01 22:10:13 -0300

    add Migrations e AppDbContext

---
+++ CardapioOnline/Migrations/20241201012447_AddItemDetails.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CardapioOnline.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddItemDetails : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Descricao",
+                table: "Itens",
+                type: "TEXT",
+                maxLength: 500,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ImagemUrl",
+                table: "Itens",
+                type: "TEXT",
+                maxLength: 500,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Ingredientes",
+                table: "Itens",
+                type: "TEXT",
+                maxLength: 500,
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Descricao",
+                table: "Itens");
+
+            migrationBuilder.DropColumn(
+                name: "ImagemUrl",
+                table: "Itens");
+
+            migrationBuilder.DropColumn(
+                name: "Ingredientes",
+                table: "Itens");
+        }
+    }
+}
---
+++ CardapioOnline/Migrations/20241201134927_DataSeedingCorrection.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CardapioOnline.Migrations
+{
+    /// <inheritdoc />
+    public partial class DataSeedingCorrection : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Categorias",
+                keyColumn: "Id",
+                keyValue: 1L,
+                column: "CriadoEm",
+                value: new DateTime(2024, 12, 1, 10, 49, 26, 853, DateTimeKind.Local).AddTicks(8748));
+
+            migrationBuilder.UpdateData(
+                table: "Categorias",
+                keyColumn: "Id",
+                keyValue: 2L,
+                column: "CriadoEm",
+                value: new DateTime(2024, 12, 1, 10, 49, 26, 853, DateTimeKind.Local).AddTicks(8783));
+
+            migrationBuilder.UpdateData(
+                table: "Itens",
+                keyColumn: "Id",
+                keyValue: 2L,
+                column: "Preco",
+                value: 15.99m);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Categorias",
+                keyColumn: "Id",
+                keyValue: 1L,
+                column: "CriadoEm",
+                value: new DateTime(2024, 12, 1, 9, 46, 48, 79, DateTimeKind.Local).AddTicks(4363));
+
+            migrationBuilder.UpdateData(
+                table: "Categorias",
+                keyColumn: "Id",
+                keyValue: 2L,
+                column: "CriadoEm",
+                value: new DateTime(2024, 12, 1, 9, 46, 48, 79, DateTimeKind.Local).AddTicks(4407));
+
+            migrationBuilder.UpdateData(
+                table: "Itens",
+                keyColumn: "Id",
+                keyValue: 2L,
+                column: "Preco",
+                value: 0m);
+        }
+    }
+}
